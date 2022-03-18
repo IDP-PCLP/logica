b830cd7055b8d459df7248300cf9ea294d8f5668
IDP-PCLP <83246996+IDP-PCLP@users.noreply.github.com>
2022-03-17 23:55:22 -0300

    Add files via upload

---
+++ scripts/aula 1/input0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Olá, mundo!
+Created on Wed Mar 16 00:42:15 2022
+
+@author: cafe
+"""
+
+input("Como você se chama? ")
---
+++ scripts/aula 1/input1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Olá, mundo!
+Created on Wed Mar 16 00:42:15 2022
+
+@author: cafe
+"""
+
+nome = input("Como você se chama? ")
+print(f"Olá, {nome}")
